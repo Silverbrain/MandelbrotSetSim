d5e7303e94cc41cad45e5df40a916e28f910f043
Sina Ataei <sinaataei@outlook.com>
2024-11-23 15:42:58 +0000

    Refactor mandelbrot_mw.c remove commented out code

--- mandelbrot_mw.c
+++ mandelbrot_mw.c
@@ -10,65 +10,8 @@ Notes:
 #include <stdio.h>
 #include <complex.h>
 #include <stdbool.h>
-#include <math.h>
 #include <mpi.h>
 #include "mandelbrot_mw.h"
-
-/* Number of intervals on real and imaginary axes*/
-// #define N_RE 12000
-// #define N_IM 8000
-
-/* Number of iterations at each z value */
-//int nIter[N_RE + 1][N_IM + 1];
-
-/* Points on real and imaginary axes*/
-//float z_Re[N_RE + 1], z_Im[N_IM + 1];
-
-/* Domain size */
-// const float z_Re_min = -2.0; /* Minimum real value*/
-// const float z_Re_max = 1.0;	 /* Maximum real value */
-// const float z_Im_min = -1.0; /* Minimum imaginary value */
-// const float z_Im_max = 1.0;	 /* Maximum imaginary value */
-
-/* Set to true to write out results*/
-// const bool doIO = true;
-// const bool verbose = true;
-
-/******************************************************************************************
-	Calculate number of iterations for a given i index (real axis) and loop over all j values
-	(imaginary axis). The nIter array is populated with the results.
-******************************************************************************************/
-// void calc_vals(int i)
-// {
-
-// 	/* Maximum number of iterations*/
-// 	const int maxIter = 100;
-
-// 	/* Value of Z at current iteration*/
-// 	float complex z;
-// 	/*Value of z at iteration zero*/
-// 	float complex z0;
-
-// 	int j, k;
-
-// 	/* Loop over imaginary axis */
-// 	for (j = 0; j < N_IM + 1; j++)
-// 	{
-// 		z0 = z_Re[i] + z_Im[j] * I;
-// 		z = z0;
-
-// 		/* Iterate up to a maximum number or bail out if mod(z) > 2 */
-// 		k = 0;
-// 		while (k < maxIter)
-// 		{
-// 			nIter[i][j] = k;
-// 			if (cabs(z) > 2.0)
-// 				break;
-// 			z = z * z + z0;
-// 			k++;
-// 		}
-// 	}
-// }
 
 //****************************/// WARNING ///**********************************
 //	The following function is obsolete due to the new implementation method
@@ -135,80 +78,6 @@ void do_communication(int myRank)
 	}
 	MPI_Group_free(&workerGroup);
 }
-
-/******************************************************************************************/
-
-// void write_to_file(char filename[])
-// {
-// 	int i, j;
-// 	FILE *outfile;
-
-// 	outfile = fopen(filename, "w");
-// 	for (i = 0; i < N_RE + 1; i++)
-// 	{
-// 		for (j = 0; j < N_IM + 1; j++)
-// 		{
-// 			fprintf(outfile, "%f %f %d \n", z_Re[i], z_Im[j], nIter[i][j]);
-// 		}
-// 	}
-// 	fclose(outfile);
-// }
-
-/******************************************************************************************
-Initialise the input array with zero to avoid arbiterary values
-******************************************************************************************/
-// void init_arr()
-// {
-// 	nIter = (int**)malloc(sizeof(int*) * (N_RE + 1));
-//     for (int i = 0; i < N_RE + 1; i++)
-//     {
-//         nIter[i] = (int*)calloc((N_IM + 1), sizeof(int));
-//     }
-//     z_Re = (float*)calloc((N_RE + 1), sizeof(float));
-//     z_Im = (float*)calloc((N_IM + 1), sizeof(float));
-// }
-
-/******************************************************************************************
-Calculate points on real axis
-******************************************************************************************/
-// void calc_pnt_on_RE_axs()
-// {
-// 	for (int i = 0; i < N_RE + 1; i++)
-// 	{
-// 		z_Re[i] = (((float)i) / ((float)N_RE)) * (z_Re_max - z_Re_min) + z_Re_min;
-// 	}
-// }
-
-/******************************************************************************************
-Calculate points on imaginary axis
-******************************************************************************************/
-// void calc_pnt_on_IM_axs()
-// {
-// 	for (int i = 0; i < N_IM + 1; i++)
-// 	{
-// 		z_Im[i] = (((float)i) / ((float)N_IM)) * (z_Im_max - z_Im_min) + z_Im_min;
-// 	}
-// }
-
-/******************************************************************************************
-Unpack recieved message to extract calculated column and next process
-******************************************************************************************/
-// void unpack_msg(int *nextProc, int *res_buff, int proc_rank, int col_idx)
-// {
-// 	/* Extracting the rank of nextProc and index of computed column from message buffer*/
-// 	*nextProc = res_buff[proc_rank];
-// 	int calc_col = res_buff[col_idx];
-
-// 	// Check if the column index is valid
-// 	if (calc_col >= 0)
-// 	{
-// 		// Unpack the message buffer and store the received column in the nIter array
-// 		for (int j = 0; j < N_IM + 1; j++)
-// 		{
-// 			nIter[calc_col][j] = res_buff[j];
-// 		}
-// 	}
-// }
 
 /******************************************************************************************/
 
